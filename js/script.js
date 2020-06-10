3b0092c6df8de1184ee579b8f90209b5545ec7d7
AndriiChemerys <andriichemerys@gmail.com>
2020-06-10 20:23:31 +0200

    Creating responsive navbar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const toggleBtn = document.querySelector('.toggle-button')
-const navbarLinks = document.querySelector('.header__nav')
+const navbarLinks = document.querySelector('.navbar__links')
 
 toggleBtn.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
